e21d13bf12394a716ead8a22cbc049d158e32e62
Deni Chen <Deni.Chen@mdrc.org>
2017-10-10 11:56:54 -0400

    Adding the items that the monte carlo simulations call from: gen.blocked.data.R, adjust.WY.R, and functions.R

---
+++ adjust.WY.R
@@ -0,0 +1,116 @@
+
+### Functions
+### trimmed to just perm.regs and adjust.WY for this project.
+
+### resamp.by.block function to help resample for fixed cases
+
+resamp.by.block<-function(...) {
+  tc<-numeric(...[1])
+  tc[1:(...[1]*...[2])]<-1
+  sample(tc,replace=FALSE)  
+}
+
+
+### perm.regs: performs regressions with permuted treatment indicator
+#Mdata is a dataset for one domain (m) for one sample, subgroups=NULL, which.mult="pooled", incl.covar-> unnecessary
+#clustered=TRUE, clusterby='block.id', funct= 'random' or 'fixed'
+perm.regs <- function(permT,data,clusterby,funct,maxT,n.j,J) {
+  outpt<-numeric(length(data))
+  M<-length(data)
+  for (m in 1:M) {
+    Mdata<-data[[m]]
+    Mdata$Treat.ij <- permT
+    mdum <- make.dummies(Mdata,clusterby=clusterby,n.j=n.j,J=J) # took this out of perm.reg so doing just once
+    fit <- make.model(mdum$fixdat, mdum$dnames, funct)
+    ifelse(maxT,outpt[m]<-get.tstat(fit),outpt[m]<-get.pval(fit))
+    }
+  return(outpt)
+}
+
+### comp.rawp: makes nullp and rawp comparisons and returns indicators
+# nullprow: row of p-values
+# rawp: raw p-values
+# r.m.r: vector of indices of rawp in order
+# The length of nullprow, rawp, and r.m.r are the same. This function is performed on each row of nullp.mat
+
+comp.rawp <- function(nullprow, rawp, r.m.r) {
+  num.test <- length(nullprow)
+  minp <- rep(NA, num.test)
+  minp[1] <- min(nullprow[r.m.r]) < rawp[r.m.r][1]
+  for (h in 2:num.test) {
+    minp[h] <- min(nullprow[r.m.r][-(1:(h-1))]) < rawp[r.m.r][h]
+  }
+  return(as.integer(minp))
+}
+comp.rawt <- function(nullptrow, rawt, r.m.r) {
+  num.test <- length(nullptrow)
+  maxt <- rep(NA, num.test)
+  maxt[1] <- max(abs(nullptrow)[r.m.r]) > abs(rawt)[r.m.r][1]
+  for (h in 2:num.test) {
+    maxt[h] <- max(abs(nullptrow)[r.m.r][-(1:(h-1))]) > abs(rawt)[r.m.r][h]
+  }
+  return(as.integer(maxt))
+}
+#call to adjust.WY from get.adjp
+#adjust.WY(data=mdat, B=B, subgroup=NULL, which.mult="pooled", incl.covar=TRUE, rawp=rawp, ncl=ncl, clustered=TRUE, clusterby='block.id', funct)
+# adjust.WY: does WY adjustments for a single sample in parallel
+adjust.WY<-function(data, B, rawp, rawt, ncl, clustered, clusterby, funct, maxT) {
+  
+  # get number of tests
+  ntests <- length(data)
+  
+  # get order of raw p-values; returns ordered index for the vector "rawp"
+  ifelse(maxT==FALSE, r.m.r <- order(rawp), r.m.r <- order(abs(rawt), decreasing=TRUE))
+  
+  # permute all Treatment indicator B times (outside of apply below so all outcomes have same permutation)
+  permT<-matrix(NA,n.j*J,B)
+  for (b in 1:B) {
+    permT[,b]<-as.vector(apply(cbind(n.j,p.j),1,resamp.by.block))
+    summary(permT[,b])
+  }
+
+  
+  cl <- makeSOCKcluster(rep("localhost", ncl))
+  clusterExport(cl, varlist=c("perm.regs", "data", "clustered", "clusterby", "funct", "make.dummies", "make.model", "get.tstat", "get.pval", "p.j", "resamp.by.block", "fastLm", "lmer"), envir=environment())
+  
+  # get null p-values (if maxT=FALSE) or test-statistics (if maxT=TRUE) using permuted T's
+  nullpt <- parApply(cl,permT,2,perm.regs,data=data,clusterby=clusterby,funct=funct,maxT=maxT,n.j=n.j,J=J)   # revised KP
+
+  stopCluster(cl)
+  
+#   # get B p-values for all tests
+#   nullpt <- lapply(data, function(y) {
+#  #   parSapply(cl, 1:B, function(x) { #in parallel
+#     sapply(1:B, function(x) { #in serial
+#       #perm.regs <- function(Mdata, Subgroups, which.mult, incl.covar, clustered, clusterby, funct) --> inputs for perm.regs
+#       #perm.regs calls make.dummies, make.model, and get.pval
+#       perm.regs(y, clustered=clustered, clusterby=clusterby, funct=funct, maxT=maxT) # y is dataset for one outcom
+#     })
+#   })
+  
+  # turn nullpt into a matrix (B rows, ntest columns)
+  nullpt.mat <- t(nullpt) # revised KP
+  #nullpt.mat <- do.call(cbind, nullpt)
+  
+  # create dummies for comparisions of null p-values to raw p-values
+ if (maxT==FALSE) ind.B <- apply(nullpt.mat, 1, comp.rawp, rawp=rawp, r.m.r=r.m.r)
+ if (maxT==TRUE) ind.B <- apply(nullpt.mat, 1, comp.rawt, rawt=rawt, r.m.r=r.m.r)
+
+  
+  # take means of dummies, these are already ordered (by r.m.r) but still need to enforce monotonicity
+  pi.p.m <- rowMeans(ind.B)
+  
+  # enforce monotonicity (keep everything in same order as sorted RAW pvalues from original data)
+  adjp.minp <- numeric(ntests)
+  adjp.minp[1] <- pi.p.m[1]
+  for (h in 2:length(pi.p.m)) {
+    # adjp.minp is a numeric vector of 0's, it will always be less than the values in pi.p.m, right?
+    adjp.minp[h] <- max(pi.p.m[h], adjp.minp[h-1])
+  }
+  
+  out <- cbind(rawp[r.m.r], adjp.minp, r.m.r)
+  colnames(out) <- c("rawp", "WY", "test num")
+  oo <- order(out[ ,"test num"])
+  out.oo <- out[oo, ]
+  return(out.oo)
+}
---
+++ functions.R
@@ -0,0 +1,290 @@
+###########################################################################
+#  FUNCTIONS FOR EC-METHODS: calculating p-vals, power  		              #
+#												                                                  #
+#	Contents: 	make.dummies; make.model; get.pval; get.rawp		            #
+#							get.output, get.adjp, get.rejects					                  #
+###########################################################################
+dir <- getwd()
+source(paste0(dir,"/gen.blocked.data.r"))
+###########################################################################
+#	Function: make.dummies		Inputs:	dat, clusterby, n.j, J	              #
+#		a dataset (dat), 								                                      #
+#		a column name as a string to cluster by (clusterby), 		              #
+#		n.j, and J 									                                          #
+#	Outputs: dummies (column names), lmedat.fixed (data.frame)		          #
+###########################################################################
+
+make.dummies <- function(dat, clusterby, n.j, J){
+  block.rep<-matrix(data=rep(dat[,clusterby],n.j),
+                    nrow=length(dat[,clusterby]),ncol=J)
+  colnum<-seq(1:J)
+  block.dum.fn<-function(...) {1*(...==colnum)}
+  block.dum<-t(apply(block.rep,1,block.dum.fn))
+  colnames(block.dum)<-paste("block",1:J,sep="")
+  lmedat.fixed<-cbind(dat,block.dum)
+  dummies<-paste(colnames(block.dum[,-1]),collapse="+")
+  
+  return(list("dnames"=dummies, "fixdat"=lmedat.fixed))
+}
+
+###########################################################################
+#	Function: make.model	Inputs:dat,dummies,funct	                        #
+#		a reshaped dataset (dat)-->data for one m 			                      #
+#		dummies, string of dummy names in formula, if function needs          #
+#		funct, the model type to use as string				                        #
+#         fixfastLm, fixlmer, or random                                   #
+#	Outputs: model object of type function					                        #
+#	Notes: dummies can be output of make.dummies $dnames			              #
+###########################################################################
+
+# make.model <- function(dat, dummies, funct){
+#   if (funct == 'fixfastLm') {
+#     form <- as.formula(paste0("D~Treat.ij+Covar.j+Covar.ij+", dummies))
+#     mod <- fastLm(form, data=dat)
+#   } else {
+#     if (funct == 'fixlmer') { 
+#       form <- as.formula("D~Treat.ij+Covar.j+Covar.ij+(1|block.id)")
+#     }
+#     else if (funct == 'random') {
+#       form <- as.formula(paste0("D~Treat.ij+Covar.j+Covar.ij+(Treat.ij|block.id)"))   
+#     }
+#     suppressMessages(mod <- lmer(form, data=dat))
+#   }
+#   return(mod)
+# }
+
+###########################################################################
+#	Function: get.pval 	Inputs: mod						                              #
+#		a model object, mod							                                      #
+#	Outputs: pvalue 									                                      #
+###########################################################################
+
+get.pval <- function(mod) {
+  if(class(mod)=="lmerMod") {
+     pval <-(1-pnorm(abs(summary(mod)$coefficients["Treat.ij","t value"])))*2
+  } else { #if class(mod) is "fastLm"
+    pval <- summary(mod)$coef["Treat.ij", "Pr(>|t|)"]
+  }
+  return(pval)
+}
+
+get.tstat <- function(mod) {
+  if(class(mod)=="lmerMod") {
+    tstat <-summary(mod)$coefficients["Treat.ij","t value"]
+  } else { #if class(mod) is "fastLm"
+    tstat <- summary(mod)$coef["Treat.ij", "t value"]
+  }
+  return(tstat)
+}
+
+
+###########################################################################
+#	Function: get.rawp	Inputs: mdat, funct, n.j, J      	                  #                                       			 
+#   mdat, a single dataset from list of S datasets as a list length M     #
+#		funct, a string "random", "fixfastLm", or "fixlmer"					          #
+#		n.j and J, number of obs at a site and number of sites, respectively	#
+#												                                                  #
+# Calls: make.dummies, make.model, get.pval                               #
+#	Outputs: matrix of raw p-values for a single sample		                  #
+#	Notes: gets raw p-vals for a single dataset and funct at a time	        #
+###########################################################################
+
+get.rawp <- function(mdat, funct, n.j, J) {
+  if (funct == "random") {mods = lapply(mdat, function(m) make.model(m, NULL, funct))}
+  else { #if funct == "fixfastLm" or "fixlmer"
+    mdums = lapply(mdat, function(m) make.dummies(m, "block.id", n.j, J))
+    mods = lapply(mdums, function(m) make.model(m$fixdat, m$dnames, funct))
+  }
+  #mods is a list of m models
+  rawp = sapply(mods, function(x) get.pval(x))
+  return(rawp)
+}
+
+get.rawt <- function(mdat, funct, n.j, J) {
+  if (funct == "random") {mods = lapply(mdat, function(m) make.model(m, NULL, funct))}
+  else { #if funct == "fixfastLm" or "fixlmer"
+    mdums = lapply(mdat, function(m) make.dummies(m, "block.id", n.j, J))
+    mods = lapply(mdums, function(m) make.model(m$fixdat, m$dnames, funct))
+  }
+  #mods is a list of m models
+  rawt = sapply(mods, function(x) get.tstat(x))
+  return(rawt)
+}
+
+# get.rawp <- function(mdat, funct, n.j, J) {
+#   if (funct == "fixed") {
+#     mdums = lapply(mdat, function(m) make.dummies(m, "block.id", n.j, J))
+#     mods = lapply(mdums, function(m) make.model(m$fixdat, m$dnames, funct))
+#   }
+#   if (funct == "random") {mods = lapply(mdat, function(m) make.model(m, NULL, funct))}
+#   #mods is a list of m models
+#   rawp = sapply(mods, function(x) get.pval(x))
+#   return(rawp)
+# }
+
+
+###########################################################################
+#  Function: make.samples	Inputs: inputs for gen.blocked.data, funct, S   #
+#		parameters for gen.blocked data                                       #
+#		funct, a string "random", "fixfastLm", "fixlmer"				              #
+#		S, number of samples                                                	#
+#												                                                  #
+# Calls: gen.blocked.data                                                 #
+#	Outputs: list of S datasets                 				                    #
+#	Notes: checks that inputs are properly specified              	        #
+###########################################################################
+
+# make.samples <- function(M,DMDES,n.j,J,rho.0,rho.1,theta,ICC,alpha, Gamma.00,sig.sq, p.j.range, R2.1, R2.2, check, omega, funct, S) {
+#   #many of these inputs need to be vectors of length M, check this and break if not true:
+#   stopifnot(length(MDES)==M, length(Gamma.00)==M, length(sig.sq)==M, length(omega)==M|omega==NULL, length(R2.1)==M, length(R2.2)==M, length(ICC)==M)
+#   #if the model is fixed effects, then ICC, R2.2 equal 0 and omega equals null, this checks that for all M:
+#   if(funct=="fixed") {stopifnot(R2.2==0, ICC==0, omega==NULL)}
+#   #if everything above is ok, generate s datasets, return a list length S with each entry a dataset
+#   samples <- lapply(1:S, function(x) gen.blocked.data(M,DMDES,n.j,J,rho.0,rho.1,theta,ICC,alpha, Gamma.00,sig.sq, p.j.range, R2.1, R2.2, check, omega))
+# }
+
+###########################################################################
+#  Function: makelist.samp  Inputs:   M, samp                             #
+#		    M, number of domains                                              #
+#		    samp, a single sample of data                                     #
+#                                                                         #
+#	Outputs: list length M of data by domain                 				        #
+#	Notes:                                                        	        #
+###########################################################################
+
+makelist.samp <-function(M, samp) {
+  #a list length M for a sample of data, each entry is a dataset for a single domain
+  mdat <- lapply(1:M, function(m) samp[,c("block.id", "Treat.ij", grep(as.character(m), names(samp), value=TRUE))])
+  mdat <- lapply(mdat, function(x) 
+    data.frame(D=x[,grep("D.M", names(x), value=TRUE)], #outcome
+               Covar.j = apply(x[,grep("X[0-9].j", names(x), value=TRUE)], 1, function(y) y[1]*y[2]), #site level covariate
+               Covar.ij = apply(x[,grep("X[0-9].ij", names(x), value=TRUE)], 1, function(h) h[1]*h[2]), #individual level covariate
+               Treat.ij = x[,"Treat.ij"],
+               block.id = x[,"block.id"]))
+  return(mdat)
+}
+
+
+###########################################################################
+#	Function: est.power			Inputs: procs, funct, S, B, gen.blocked.data    #
+#		procs, a vector of strings for adjustment procedures		 	            #
+#		funct, a string "random", "fixfastLm", or "fixlmer"                   #
+#   S, number of samples for power calc                                   #
+#   B, number of permutations for WY                                      #
+#   all other inputs for gen.blocked.data                                 #
+#												                                                  #
+# Calls:  get.adjp, get.rejects, gen.blocked.data, makelist.samp,         #
+#         get.rawp                                                        #
+#	Outputs:  a matrix of power calculations                                #
+#	Notes: 	                              		                              #
+###########################################################################
+
+est.power <- function(procs, M,DMDES,n.j,J,rho.0_lev1, rho.0_lev2,rho.1_lev2,theta,ICC,alpha, Gamma.00,sig.sq, p.j.range, R2.1, R2.2, check, omega, funct, S, B, maxT) {
+  if (M==1) {
+    print("Multiple testing corrections are not needed when M=1")
+    procs <- NULL
+  }
+#   if (funct=="random" & any(grepl("WY", procs))) {
+#     print("Adjust.WY code not currently set up for random effects models")
+#     procs<-procs[-grep("WY", procs)]
+#   }
+  power.results <- matrix(NA, nrow=(length(procs)+1), ncol=M+5)
+  colnames(power.results) = c(paste0("D", 1:M, "indiv"), "min", "1/3", "1/2","2/3", "full")
+  rownames(power.results) = c("rawp", procs)
+  
+  nulls <-which(DMDES==0)
+  alts <-which(DMDES!=0)
+  
+  adjp.proc <- array(0, c(S, M, length(procs)+1))
+  dimnames(adjp.proc) <- list(NULL, NULL, c("rawp", procs))
+  
+  names(adjp.proc) <- c("rawp", procs)
+  
+  px<-S/100
+  rawt.all<-matrix(NA,S,M)
+  for (s in 1:S) {
+    t1<-Sys.time()
+    if (s%%px==0){print(paste0("Now processing sample ", s, " of ", S))}
+    samp <- gen.blocked.data(M,DMDES,n.j,J,rho.0_lev1, rho.0_lev2,rho.1_lev2,theta,ICC,alpha,Gamma.00,sig.sq,p.j.range,R2.1,R2.2,check,omega)
+    #if (s==1) print(c(sd(samp$D0.M1.ij),sd(samp$D.M1.ij)))
+    mdat <- makelist.samp(M, samp) #list length M
+    rawp <- get.rawp(mdat, funct, n.j, J) #vector length M
+    rawt <- get.rawt(mdat, funct, n.j, J) #vector length M
+    rawt.all[s,]<-rawt
+    for (p in 1:(length(procs)+1)) {
+      if (p==1) {
+        pvals <-rawp
+        proc <- "rawp"
+      } else {
+        t11<-Sys.time()
+        proc <- procs[p-1]
+        pvals <- get.adjp(rawp, rawt, proc, alpha, B, ncl, mdat, maxT)
+        t21<-Sys.time()
+        if (s==1) {print(paste("One sample of ", proc, " took ", t21-t11))}
+      }
+      adjp.proc[s,,proc] = pvals
+    }
+    t2<-Sys.time()
+   if (s==1) {print(paste("Expected time diff of", (t2-t1)*S, "and expected finish at", t1+(t2-t1)*S,"for S =", S, sep=" "))}
+   else if (s%%px==0) {print(difftime(t2, t1))}
+  }
+  for (p in 1:(length(procs)+1)) {
+    if (M==1) {power.results[p,1:M]<-mean(adjp.proc[,,p]<alpha)}
+    else {power.results[p, 1:M] <- apply(adjp.proc[,,p], 2, function(x) mean(x<alpha))}
+    rejects <- get.rejects(adjp.proc[,,p], alpha)
+    if (M==1) {
+      if (alts!=0) num.t.pos=rejects
+    }
+    else if (length(alts)==1) {num.t.pos<-rejects[,alts]}
+    else {num.t.pos <- apply(rejects[,alts], 1, sum)}
+    power.results[p, "min"] <- mean(1*(num.t.pos>0))
+    power.results[p, "1/3"] <- mean(1*(num.t.pos>=(1/3)*M))
+    power.results[p, "1/2"] <- mean(1*(num.t.pos>=(1/2)*M))
+    power.results[p, "2/3"] <- mean(1*(num.t.pos>=(2/3)*M))
+    power.results[p, "full"] <- mean(1*(num.t.pos==M))
+  }
+  return(power.results)
+}
+
+
+###########################################################################
+#  Function: get.adjp 	Inputs: rawp, proc, alpha				                  #
+#		a matrix nrow=S ncol=M of raw p-values							                  #
+#  	a string for a single proc            							                  #
+#  	a number, alpha, should be 0.05 in most cases	 			                  #
+#	Outputs: MxS matrix of adjusted p-values for a single proc 	            #
+###########################################################################
+
+get.adjp <- function(rawp, rawt, proc, alpha, B, ncl, mdat, maxT) {
+  if(proc=="WY"){
+    #adjust.WY<-function(data, B, subgroup, which.mult, incl.covar, rawp, ncl, clustered, clusterby, funct)
+    #print(paste0("working on ", proc, " with ", B, " permutations"))
+    #adjust.wy needs rawp as a vector for all m of a single sample
+    tw1 <- Sys.time()
+    adjp.proc <- adjust.WY(data=mdat, B=B, rawp=rawp, rawt=rawt, ncl=ncl, clustered=TRUE, clusterby='block.id', funct, maxT)[,"WY"]
+    tw2 <- Sys.time()
+   # print(difftime(tw2, tw1))
+  }
+  else {
+    #return a matrix with m columns (domains) and b rows (samples)
+    #this needs rawp to be a matrix with m columns and was designed for all samples to be a row.
+    mt.out <-mt.rawp2adjp(rawp, proc, alpha)
+    adjp.proc <- mt.out$adjp[order(mt.out$index), proc]
+      #mt.rawp2adjp(rawp, proc, alpha)$adjp[,proc]
+  }
+  return(adjp.proc)
+}
+
+###########################################################################
+#  Function: get.rejects   Inputs: adjp, alpha  				                  #
+#		a matrix nrow=S ncol=M of adjusted p-values, from get.adjp			      #
+#  	a number, alpha, should be 0.05 in most cases	 			                  #
+#	Outputs: MxS matrix of 1 and 0 indicating rejecting the null            #
+###########################################################################
+
+get.rejects <- function(adjp, alpha) {
+  #return a matrix of 1 and 0 (for true/false <alpha)
+  rejects <- 1*(adjp<alpha)
+}
+
+
