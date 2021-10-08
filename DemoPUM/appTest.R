f8c5f1426693f1a77ec71bc071661a923c7b1aaa
Zarni Htet <htet@rserver02.corpmdrc.org>
2021-10-08 11:06:53 -0400

    Functionalizing UI elements etc

--- DemoPUM/appTest.R
+++ DemoPUM/appTest.R
@@ -23,7 +23,7 @@ ui <- fluidPage(
               tabPanel("Educational Resources"),
               tabPanel("Power Calculation", 
                 tabsetPanel(id = "subMenu",
-                  tabPanel("Single Scenario",
+                  tabPanel(id = "Single Scenario",
                     sidebarLayout(
                       sidebarPanel(
                         # css to center the progress bar
@@ -68,7 +68,8 @@ ui <- fluidPage(
                                 selectInput("designP2LBISS", "What Research Design is this for?", 
                                              choices = list("Two Level Blocked RCT - Constant Effects" = "d2.1_m2fc", 
                                                             "Two Level Blocked RCT - Fixed Effects" = "d2.1_m2ff", 
-                                                            "Two Level Blocked RCT - Random Effects" = "d2.1_m2fr"))) # select input buttons div
+                                                            "Two Level Blocked RCT - Random Effects" = "d2.1_m2fr"),
+                                             selected = "d2.1_m2ff")) # select input buttons div
                                 ), # column for inputs
                                            
                             column(2, 
@@ -87,11 +88,29 @@ ui <- fluidPage(
                                       placement = "right", 
                                       trigger = "hover", 
                                       options = list(container = "body")), # the bsPopover for the more information section of the Shiny App
-                            
+                          
                             fluidRow(
-                              column(12,
-                                     uiOutput("MTP"))
-                            ),
+                                column(12,
+                                 uiOutput("MTP"))
+                            ), # MTP shared by all designs
+                        
+                           fluidRow(
+                                column(12,
+                                 uiOutput("M"))
+                           ), # Number of Outcomes
+                        
+                           # fluidRow(
+                           #      column(12,
+                           #       uiOutput("MDES"))
+                           # ), # Minimum Detectable Effect Size
+      
+                           conditionalPanel(condition = c("input.designP2LBISS == 'd2.1_m2ff' || input.designP2LBISS == 'd2.1_m2fr'"),
+                              fluidRow(
+                                column(12,
+                                  uiOutput("R2.1"))
+                              ) # Adding R2.1 which does not exist in 1 level constant effect
+   
+                           ), # conditional Panel for the 2 level constant & fixed effects
                               
                             fluidRow(
                               column(6,
@@ -136,30 +155,222 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output, session = FALSE) {
   
-  # get reactive expression for design
+  ##########################################################
+  # Get reactive expression for experimental chosen design #
+  ##########################################################
+  
   getDesign <- reactive({
     
     if(input$designP2LBISS == "d2.1_m2ff") {
       
+      print("d2.1_m2ff")
       return(c("d2.1_m2ff"))
       
     } else if(input$designP2LBISS == "d2.1_m2fr"){
       
+      print("d2.1_m2fr")
       return(c("d2.1_m2fr"))
+      
+    } else if(input$designP2LBISS == "d2.1_m2fc") {
+      
+      print("d2.1_m2fc")
+      return(c("d2.1_m2fc"))
       
     }
   
-  })
+  }) # getDesign
   
-  #RenderUI for the selector input for different power definitions
+  ######################################################
+  # Rendering Variable Objects to UI for chosen design #
+  ######################################################
+  
   output$MTP <- renderUI({
     
     theDesign = as.character(getDesign())
-    # browser()
     div(style = "display: inline-block, vertical-align:top;", 
-        mtpInput(design = theDesign , scenario = "singleblock"))
+        mtpInput(design = theDesign , scenario = "singlescenario"))
     
-  }) #power select input options
+  }) # MTP for chosen design
+  
+  output$M <- renderUI({
+    
+    theDesign = as.character(getDesign())
+    div(style = "display: inline-block, vertical-align:top;", 
+        mInput(design = theDesign, scenario = "singlescenario"))
+  }) # Number of Outcomes 
+  
+  # output$MDES <- renderUI({
+  #   
+  #   theDesign = as.character(getDesign())
+  #   div(style = "display: inline-block, vertical-align:top:",
+  #       mdesInput(design = theDesign, scenario = "singlescenario"))
+  # }) # Minimum detectable effect size
+
+  output$R2.1 <- renderUI({
+    
+    theDesign = as.character(getDesign())
+    div(style = "display: inline-block, vertical-align:top;", 
+        r2.1Input(design = theDesign, scenario = "singlescenario"))
+  
+  }) # R2.1 element for chosen and required designs
+  
+  
+  observeEvent(input$goButtonP2LBISS,{
+    
+    # set a Reactive Value for Power Table
+    reactPowerTable <- reactiveVal()
+    
+    #########################
+    # Creating Progress bar #
+    #########################
+    
+    progress <- shiny::Progress$new()
+    progress$set(message = "Calculating Power", value = 0)
+    # Close the progress bar when this reactive expression is done (even if there is an error)
+    on.exit(progress$close())
+    
+    # Update Progress Bar Callback function
+    updateProgress <- function(value = NULL, detail = NULL, message = NULL){
+      
+      if (is.null(value)){
+        
+        value <- progress$getValue()
+        value <- value + (progress$getMax() - value)/5
+        
+      } # Progess bar in terms of values' increments
+      
+      progress$set(value = value, detail = detail, message = message)
+      
+    } # End of Callback Progress Function
+    
+    ############################
+    # Generating Power Results #
+    ############################
+    
+    #browser()
+    dat <- as.data.frame(
+      isolate(pum::pump_power(design = input$designP2LBISS,
+                              MTP = as.character(unlist(strsplit(input$MTPP2LBISS," "))),
+                              MDES = as.numeric(unlist(strsplit(input$MDESP2LBISS, ","))),
+                              M = input$MP2LBISS, # The number of hypotheses/outcomes
+                              J = input$JP2LBISS, # The number of schools
+                              K = input$KP2LBISS, # The number of districts
+                              nbar = input$nbarP2LBISS, # The number of units per block
+                              Tbar = input$tbarP2LBISS, # The proportion of samples that are assigned to the treatment
+                              alpha = input$alphaP2LBISS,
+                              numCovar.1 = input$numCovar.1P2LBISS,
+                              numCovar.2 = 0,
+                              numCovar.3 = 0,
+                              R2.1 = rep(input$R2.1P2LBISS,
+                                         input$MP2LBISS),
+                              R2.2 = rep(0.1, input$MP2LBISS),
+                              R2.3 = rep(0.1, input$MP2LBISS),
+                              ICC.2 = rep(0, input$MP2LBISS),
+                              ICC.3 = rep(0.2, input$MP2LBISS) ,
+                              rho = input$rhoP2LBISS,
+                              omega.2 = 0,
+                              omega.3 = 0.1,
+                              tnum = 10000,
+                              B = 100,
+                              cl = NULL,
+                              updateProgress = updateProgress)
+     ))
+    
+    # Save the reactive Power Table
+    reactPowerTable(dat)
+    {reactPowerTable()}
+    
+    #################################################
+    # Rendering Single Scenario Power Table Results #
+    #################################################
+    
+    # Rendering a reactive object table from the power function
+    output$powercalcTableP2LBISS <- renderDataTable({
+      
+      DT::datatable(dat,
+                    extensions = 'Buttons',
+                    options = list(
+                      paging = TRUE,
+                      pageLength = 5,
+                      scrollY = TRUE,
+                      dom = 'Bfrtip',
+                      buttons = c('csv', 'excel')
+                    ))
+    })# Wrapping a reactive expression to a reactive table object for output view
+    
+    ############################
+    # Data preparation for plot #
+    ############################
+    dat <- reactPowerTable()
+    
+    singleScenario2LevelBlockedDatLong <- 
+      dat %>%
+      dplyr::select_all() %>%
+      dplyr::select(-indiv.mean) %>%
+      tidyr::pivot_longer(!MTP, names_to = "powerType", values_to = "power")
+    
+    ################################################
+    # Rendering Single Scenario Power Table Graphs #
+    ################################################
+    
+    output$powercalcGraphP2LBISS <- renderPlotly({
+      
+      pg <- plotly::ggplotly(
+        ggplot(
+          data = singleScenario2LevelBlockedDatLong,
+          aes(x = powerType, 
+              y = power, 
+              shape = MTP,
+              colour = MTP)) + 
+          geom_point(size = 2) +
+          scale_y_continuous(limits = c(0,1)) +
+          ggtitle("Adjusted Power values across different Power Definitions") +
+          theme(plot.title = element_text(size = 16,
+                                          face = "bold",
+                                          vjust = 1,
+                                          hjust = 0.5),
+                axis.text = element_text(size = 14),
+                axis.title = element_text(size = 14)
+          )
+        + labs(colour = "",
+               shape = "")
+      ) # End of ggplot
+      
+      pg <- layout(pg, 
+                   #title = "<b>Adjusted Power values across different \n Power Definitions & MDES values </b>",
+                   margin=list(t = 75),
+                   legend = list(x = 100,
+                                 orientation = "v",
+                                 xanchor = "center",
+                                 y = 0.5,
+                                 title = list(text = '<b> MTP Type </b>')))
+      
+      pg %>%
+        config(displaylogo = FALSE,
+               collaborate = FALSE,
+               displayModeBar = TRUE,
+               modeBarButtonsToRemove = list(
+                 'sendDataToCloud',
+                 'autoScale2d',
+                 'resetScale2d',
+                 'hoverClosestCartesian',
+                 'hoverCompareCartesian',
+                 'zoom2d',
+                 'pan2d',
+                 'select2d',
+                 'lasso2d',
+                 'zoomIn2d',
+                 'zoomOut2d',
+                 'toggleSpikelines'
+               ))
+      
+    }) # plotly graph
+    
+  }) # observe Event go Button for power for Single Scenario
+  
+  
+  
+  
   
   
   
--- DemoPUM/ui_elements.R
+++ DemoPUM/ui_elements.R
@@ -2,55 +2,15 @@
 # Objective : This document is to organize all the different UI elements for each of the design & different scenario #
 ######################################################################################################################
 
-
-
 #########################
-# Number Of MTP
+# MTP element
 #########################
-
-# mtpInput <- function(design, scenario){
-# 
-# id <- paste0("mtp", design, scenario, sep = "_")
-# question <- paste0("question", "mtp", design, scenario, sep = "_")
-#   
-# fluidRow(
-#   column(10,
-#          div(style = "display: inline-block, vertical-align:top;", 
-#              selectInput(id, "Which MTP do you plan to use?", 
-#                          choices = list("Bonferroni" = "Bonferroni", 
-#                                         "Holm" = "Holm", 
-#                                         "Benjamini-Hochberg" = "BH", 
-#                                         "Westfall-Young-Single-Step" = "WY-SS", 
-#                                         "Westfall-Young-Step-Down" = "WY-SD"),
-#                          multiple = TRUE)) # select input buttons div
-#   ), # column for inputs
-#   
-#   column(2, 
-#          div(style ="display: inline-block, 
-#                                              vertical-align:top;",
-#              actionButton(question,
-#                           label = "", 
-#                           icon = icon("question"),
-#                           style = "font-size: 10px;
-#                           argin-top: 28px;")) #div for button ends
-#   ) # column for buttons
-#   
-# ) # fluid Row to contain the question mark issue 
-# 
-# bsPopover(id = question, 
-#           title = NULL,
-#           content = paste0("For more information on MTP, please click!"),
-#           placement = "right", 
-#           trigger = "hover", 
-#           options = list(container = "body")) # the bsPopover for the more information section of the Shiny App
-#   
-# } # mtp input
 
 mtpInput <- function(design, scenario){
   
-  id <- paste0("mtp", design, scenario, sep = "_")
+  id <- paste0("mtp", "_", design, "_" , scenario)
   
-  selectInput(id, "Which MTP do you plan to use?", 
+  selectInput(id, "Multiple Testing Procedure (MTP)", 
               choices = list("Bonferroni" = "Bonferroni", 
                               "Holm" = "Holm", 
                               "Benjamini-Hochberg" = "BH", 
@@ -70,7 +30,6 @@ mtpActionButton <- function(design, scenario){
   
 } # mtpAction Button
 
-
 mtpPopOver <- function(design, scenario){
   
   question <- paste0("question", "mtp", design, scenario, sep = "_")
@@ -83,9 +42,47 @@ mtpPopOver <- function(design, scenario){
   )
 } #mtpPop over
 
+#########################
+# Number of Outcomes (M)
+#########################
 
+mInput <- function(design, scenario){
+  
+  id <- paste0("M", "_", design, "_" , scenario)
+  numericInput(id, 
+               "Number of Outcomes", 
+               min = 1, 
+               max = 10, 
+               value = 5, 
+               step = 1)
+  
+  
+} # Number of Outcomes (M)
 
+#########################
+# Minimum Detectable Effect Size
+#########################
 
+mdesInput <- function(design, scenario){
+  
+  id <- paste0("mtp", "_", design, "_" , scenario)
+  textInput(id, 
+           "Vary MDES vector (comma delimited)", 
+           value = "0.125,0.125,0.125, 0,0")
+} # mdesInput
+
+#########################
+# R2.1 element
+#########################
+
+r2.1Input <- function(design, scenario){
+  
+  id <- paste0("mtp", "_", design, "_" , scenario)
+
+  textInput(id, 
+            "Proportion of variance explained by Level-1 covariates (R2.1)", 
+            value = "0.2, 0.2, 0.2, 0.2, 0.2")
+} # R2.1 Input
 
 
 
